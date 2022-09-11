c85b93f59e3829dd748b6c42aeb0791028296dd5
KateVolynets <96291965+KateVolynets@users.noreply.github.com>
2022-09-11 14:35:23 +0300

    Update Non-unique names.py

--- Non-unique names.py
+++ Non-unique names.py
@@ -2,9 +2,6 @@ group_name = ['Elena', 'Matvei', 'Alexander', 'Bogdan', 'Inna', 'Bogdan', 'Elena
 result_group_name = []
 print('All name in group: ')
 print(group_name)
-for i in group_name:
-    if i not in result_group_name:
-        result_group_name.append(i)
 
 print('Unique name in group: ')
-print(result_group_name)
+print(set(group_name))
